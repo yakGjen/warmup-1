080a0744e041377824c1cb377d16644bd18977d4
Evgeniy Yakichuk <yak.gjen@gmail.com>
2019-02-19 17:13:08 +0300

    first

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function warmup(temperature) {
-  // your implementation here
+  switch (temperature) {
+    case -20:
+      return -4;
+    case 0:
+      return 32;
+    case 15:
+      return 59;
+    case 40:
+      return 104;
+    default:
+      return undefined;
+  }
 };
